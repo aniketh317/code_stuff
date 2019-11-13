83218df4693ed52a0b36c628bd18fbfcc3c65e93
aniketh317 <56510837+aniketh317@users.noreply.github.com>
2019-11-13 09:46:25 +0530

    Update and rename Ukkonen's suffix tree_algo to Ukkonen's suffix tree_algo.cpp

--- Ukkonen's suffix tree_algo.cpp
+++ Ukkonen's suffix tree_algo.cpp
@@ -1,6 +1,6 @@
 // String_similarity.cpp : Defines the entry point for the console application.
 //
-#include "stdafx.h"
+//#include "stdafx.h"
 #include <iostream>
 #include <vector>
 #include <string>
