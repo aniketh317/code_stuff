5479d7a30f558a523348be6da7532968e464b5cf
aniketh317 <56510837+aniketh317@users.noreply.github.com>
2020-01-02 12:56:32 +0530

    Update Push-Relabel.cpp

--- Flow-algo/Push-Relabel.cpp
+++ Flow-algo/Push-Relabel.cpp
@@ -13,6 +13,8 @@ using namespace std;
 struct res_adj {
 	int ind;//index of the node adjacent in the residual graph
 	int Cf;//residual capacity of the edge
+	res_adj(){}
+	res_adj(int i,int c):ind(i),Cf(c){}
 };
 
 struct res_node {
@@ -24,35 +26,74 @@ struct res_node {
 
 struct residual_graph {
 	vector<res_node>V;
+	int source;
+	int sink;
 
-	residual_graph(vector<vector<int>>&C)
+	residual_graph(vector<vector<int>>&C,int so,int si):source(so),sink(si)
 	{
 		int n = C.size();
 		V.resize(n);
 		for (int i = 0; i < n; i++)
 		{
 			V[i].H.resize(2 * n - 1);
-			V[i].pos.resize(n);
+			V[i].pos.resize(n,-1);
 		}
 		for (int i = 0; i < n; i++)
 		{
 			for (int j = 0; j < n; j++)
 			{
-
+				if(C[i][j] > 0)
+				{
+					if(j == source)
+					{
+						V[i].H[n].emplace_back(j,C[i][j]);//h[source] == n
+						V[i].pos[j] = V[i].H[n].size()-1;
+					}
+					
+					else
+					{
+						V[i].H[0].emplace_back(j,C[i][j]);//h[j] == 0 for j!=source
+						V[i].pos[j] = V[i].H[0].size() -1;
+					}
+				}
 			}
 		}
 	}
+	
+	
 };
 
 int max_flow(vector<vector<int>>C,int source, int sink,vector<vector<int>>F)
 //Capacity graph in the form of adjacency matrix
+//F will have preflows at every stage and finally max-flow at termination of the while loop
 {
 	int n = C.size();//number of vertices
 
 	vector<vector<int>>h(2 * n - 1);/*Stores vertices with positive excess(based on their heights)
 									maximum possible height is 2*n-1*/
 	
-	vector<res_node>RG(n);//Residual graph of size n
+	residual_graph RG(C,source,sink);//Residual graph of size n
+	for(int i=0;i<)
+	
+	bool there_is_excess = false;
+	for(int i = RG.V[source].H[0].size()-1;i>=0;i--)
+	{
+		res_adj &r = RG.V[source].H[0][i];
+		int j = r.ind;
+		int cf = r.Cf;
+		RG.V[source].H[0].pop_back();
+		F[source][j] += cf;
+		RG.V[j].exc += cf;
+		RG.V[j].H[n].emplace_back(source,cf);
+		there_is_excess = true;
+	}
+	
+	int max_exc_h = 0;//maximum height among heights of vertices with +ve excess
+	
+	while(there_is_excess)
+	{
+		
+	}
 }
 
 
@@ -60,4 +101,3 @@ int main()
 {
     return 0;
 }
-
