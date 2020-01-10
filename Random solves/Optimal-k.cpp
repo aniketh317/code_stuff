60aed5d94514e1d32430bae5da0b9cda574d8804
aniketh317 <56510837+aniketh317@users.noreply.github.com>
2020-01-10 09:54:12 +0530

    Update and rename Optimal-k to Optimal-k.cpp

--- Random solves/Optimal-k.cpp
+++ Random solves/Optimal-k.cpp
@@ -1,7 +1,7 @@
 // Selecting_Optimal-k.cpp : Defines the entry point for the console application.
 //
 
-#include "stdafx.h"
+//#include "stdafx.h"
 #include <iostream>
 #include <string>
 #include <vector>
